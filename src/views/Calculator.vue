f25dd67e82e2e26ff02c025437fbc120c2016560
daasc <paulojhol@gmail.com>
2022-09-29 17:19:01 -0300

    config

---
+++ src/views/Calculator.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>hello</h1>
+  </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Calculator",
+};
+</script>
+<style lang="scss" scoped></style>
