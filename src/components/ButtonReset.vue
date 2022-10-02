e17fa0aed88d8983a1e21644533b3a10054ff975
daasc <paulojhol@gmail.com>
2022-10-02 10:31:31 -0300

    feat: add layout

--- src/components/ButtonReset.vue
+++ src/components/ButtonReset.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="button_reset">
     <button>Reset</button>
   </div>
 </template>
@@ -14,9 +14,14 @@ button {
   color: white;
   font-weight: bold;
   border: none;
-  width: 100%;
+  width: 80%;
   padding: 8px;
   border-radius: 8px;
   cursor: pointer;
+  align-self: flex-end;
+}
+.button_reset {
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/ResultContainer.vue
@@ -0,0 +1,70 @@
+<template class="template">
+  <div class="container__result">
+    <div class="space_result">
+      <div class="content_result">
+        <div class="describe_result">
+          <span>Tip Amount</span>
+          <span>/ person</span>
+        </div>
+        <div class="number_result">$100.00</div>
+      </div>
+      <div class="content_result">
+        <div class="describe_result">
+          <span>Total</span>
+          <span>/ person</span>
+        </div>
+        <div class="number_result">$100.00</div>
+      </div>
+    </div>
+    <button-reset></button-reset>
+  </div>
+</template>
+<script>
+import ButtonReset from "./ButtonReset.vue";
+export default {
+  components: { ButtonReset },
+  name: "ResultContainer",
+};
+</script>
+<style scoped>
+
+.container__result {
+  background-color: var(--color-container-result);
+  width: 50%;
+  height: 80%;
+  border-radius: 20px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-self: center;
+}
+.space_result {
+  height: 40%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 20px;
+}
+.content_result {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.describe_result {
+  display: flex;
+  flex-direction: column;
+}
+.describe_result span:first-child {
+  color: white;
+  font-size: 1.3rem;
+}
+.describe_result span:last-child {
+  color: var(--color-font-result);
+  font-size: 1.2rem;
+}
+.number_result {
+  color: var(--color-font-result);
+  font-size: 2rem;
+}
+</style>
