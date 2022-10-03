f8d01622980de5fb42790e5383537aea1fdc66ac
daasc <paulojhol@gmail.com>
2022-10-03 17:58:01 -0300

    Static site deploy

---
+++ src/components/ButtonReset.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="button_reset">
+    <button>Reset</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "ButtonReset",
+};
+</script>
+<style scoped>
+button {
+  background-color: var(--color-button);
+  color: white;
+  font-weight: bold;
+  border: none;
+  width: 80%;
+  padding: 8px;
+  border-radius: 8px;
+  cursor: pointer;
+  align-self: flex-end;
+}
+.button_reset {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/views/Calculator.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <h1>Tip Calculator</h1>
+    <section class="container">
+      <div class="container__calculator"></div>
+      <result-container></result-container>
+    </section>
+  </main>
+</template>
+<script>
+import ResultContainer from "@/components/ResultContainer.vue";
+export default {
+  components: { ResultContainer },
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Calculator",
+};
+</script>
+<style lang="css" scoped>
+main {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+h1 {
+  text-align: center;
+}
+.container {
+  display: flex;
+  width: 50%;
+  min-height: 500px;
+  background-color: white;
+  margin: 0 auto;
+  border-radius: 20px;
+  flex-direction: row-reverse;
+  padding: 0 20px;
+}
+</style>
